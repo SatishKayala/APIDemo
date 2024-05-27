25f801987264472b07e2613485598965195623e6
SatishKayala <kayalasatish2001@gmail.com>
2024-05-27 17:49:50 +0530

    Add file

---
+++ Number.java
@@ -0,0 +1,16 @@
+class Number
+{
+  public static void main(String args[])
+  {
+   int a=10;
+   int b=20;
+   if (a>b)
+     {
+      System.out.println("The small number is b");
+     }
+    else
+     {
+      System.out.println("the small number is a");
+      }
+  }
+}
